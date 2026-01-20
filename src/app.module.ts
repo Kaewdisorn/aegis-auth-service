def6eff80fee1d559e6de72f7605cf2d4dca1a47
Nane <kds.napha@gmail.com>
2026-01-21 00:01:37 +0900

    add auth controller test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@infrastructure/logging/logger.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '@interfaces/http/auth.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     LoggerModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
