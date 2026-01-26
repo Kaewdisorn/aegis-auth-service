f121e8107bd781b5f0c969c1d648123b1207d2ef
Nane <kds.napha@gmail.com>
2026-01-26 22:37:18 +0900

    export interface to module

--- src/infrastructure/config/server-config.module.ts
+++ src/infrastructure/config/server-config.module.ts
@@ -1,8 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { AppConfigService } from './config';
+import { IAppConfig } from '@application/ports/config.interface';
 
 @Module({
-    providers: [AppConfigService],
-    exports: [AppConfigService],
+    providers: [
+        {
+            provide: IAppConfig,
+            useClass: AppConfigService,
+        }
+    ],
+    exports: [IAppConfig],
 })
 export class AppConfigModule { }
