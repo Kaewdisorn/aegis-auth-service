28b4a5b971bbc8c464d7d12cf41c9a5c3b081c7b
Nane <kds.napha@gmail.com>
2026-01-29 10:44:12 +0900

    processing Register API

--- src/interfaces/http/controllers/auth.controller.ts
+++ src/interfaces/http/controllers/auth.controller.ts
@@ -1,15 +1,14 @@
-import { BadRequestException, Controller, Post } from '@nestjs/common';
+import { RegisterUserDto } from '@application/dtos/register-user.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
 
     @Post('/')
-    // throwError() {
-    //     throw new Error('test error from AuthController');
-    // }
-    async register() {
+    async register(@Body() dto: RegisterUserDto): Promise<any> {
+        console.log(dto);
         //throw new Error('testtt error from AuthController');
-        throw new BadRequestException('Invalid input');
+        // throw new BadRequestException('Invalid input');
         // return { message: 'User registered successfully' };
     }
 }
