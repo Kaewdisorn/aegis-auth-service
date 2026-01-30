6b19d3a98f9d7961fe7f30ba40c9007ae0659dee
Nane <kds.napha@gmail.com>
2026-01-30 20:44:01 +0900

    register registerUserUseCase

--- src/interfaces/http/auth.module.ts
+++ src/interfaces/http/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
+import { RegisterUserUseCase } from '@application/use-cases/register-user.use-case';
 
 @Module({
     controllers: [AuthController],
-    providers: [],
+    providers: [RegisterUserUseCase],
 })
 export class AuthModule { }
