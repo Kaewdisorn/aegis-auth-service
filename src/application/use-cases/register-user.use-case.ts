bf9455218d22dbfa2ad17a23dc80f7cc04e8f472
Nane <kds.napha@gmail.com>
2026-01-30 20:43:40 +0900

    create RegisterUserUseCase

--- src/application/use-cases/register-user.use-case.ts
+++ src/application/use-cases/register-user.use-case.ts
@@ -1,1 +1,10 @@
-export class RegisterUserUseCase { }
+import { RegisterUserDto } from "@application/dtos/register-user.dto";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class RegisterUserUseCase {
+
+    async execute(dto: RegisterUserDto) {
+        console.log('Registering user with email:', dto.email);
+    }
+}
