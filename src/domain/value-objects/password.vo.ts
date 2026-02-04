2ef21cdbc0f0212d49f1bc8359b18b8ca7b1ce9f
Nane <kds.napha@gmail.com>
2026-02-04 21:37:18 +0900

    create value object for password

---
+++ src/domain/value-objects/password.vo.ts
@@ -0,0 +1,13 @@
+export class Password {
+
+    static create(plainPassword: string): Password {
+        // const errors = Password.validate(plainPassword);
+        // if (errors.length > 0) {
+        //     throw new Error(`Invalid password: ${errors.join(', ')}`);
+        // }
+        // return new Password(plainPassword);
+        return '';
+    }
+
+}
+
