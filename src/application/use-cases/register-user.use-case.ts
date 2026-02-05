c1cd4bd113b4828a0c9caedd09230fd5e8d33693
Nane <kds.napha@gmail.com>
2026-02-05 21:08:53 +0900

    apply password validate

--- src/application/use-cases/register-user.use-case.ts
+++ src/application/use-cases/register-user.use-case.ts
@@ -1,5 +1,6 @@
 import { RegisterUserDto } from "@application/dtos/register-user.dto";
 import { Email } from "@domain/value-objects/email.vo";
+import { Password } from "@domain/value-objects/password.vo";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
@@ -8,6 +9,8 @@ export class RegisterUserUseCase {
     async execute(dto: RegisterUserDto) {
         // Create value objects (validates format)
         const email = Email.create(dto.email);
+        Password.create(dto.password);
         console.log('Created Email VO:', email.toString());
+        console.log('Created Password VO:', Password.create(dto.password).toString());
     }
 }
