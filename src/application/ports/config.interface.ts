b18284e3ba442310c1d785fb30255ae3986802e0
Nane <kds.napha@gmail.com>
2026-02-09 22:59:22 +0900

    added interface DatabaseConfig

--- src/application/ports/config.interface.ts
+++ src/application/ports/config.interface.ts
@@ -10,10 +10,19 @@ export interface LoggerConfig {
     logDir: string;
 }
 
+export interface DatabaseConfig {
+    readonly host: string;
+    readonly port: number;
+    readonly database: string;
+    readonly username: string;
+    readonly password: string;
+}
+
 
 export interface IAppConfig {
     readonly appConfig: AppConfig;
     readonly logger: LoggerConfig;
+    readonly database: DatabaseConfig;
 }
 
 export const IAppConfig = Symbol('IAppConfig');
