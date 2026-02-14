e5b97dd4f241d2ee5d3f810d46bbe54c0f6b6f06
Nane <kds.napha@gmail.com>
2026-02-15 01:18:15 +0900

    editing UserUuid vo

--- src/domain/value-objects/user-uuid.vo.ts
+++ src/domain/value-objects/user-uuid.vo.ts
@@ -1,11 +1,11 @@
 import { generateUUIDFromString } from "src/common/utils/uuid.util";
 
-export class UserUid {
+export class UserUuid {
     private constructor(private readonly value: string) { }
 
-    static create(value: string): UserUid {
+    static create(value: string): UserUuid {
         const userId = generateUUIDFromString(value);
-        return new UserUid(userId);
+        return new UserUuid(userId);
     }
 
 
