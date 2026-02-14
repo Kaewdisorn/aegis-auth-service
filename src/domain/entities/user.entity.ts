20e3f4073f72f4e3a169976d5fa3b3c55143ec98
Nane <kds.napha@gmail.com>
2026-02-15 01:17:43 +0900

    build User entity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,4 +1,12 @@
-export class User {
+import { Email } from "@domain/value-objects/email.vo";
+import { UserUuid } from "@domain/value-objects/user-uuid.vo";
 
+export class User {
+    public readonly userGid: UserUuid;
+    public readonly userUid: UserUuid;
+    public readonly email: Email;
+    public readonly passwordHash: string;
+    public readonly createdAt: Date;
+    public readonly updatedAt: Date;
 
 }
