e57a89c9600f796d4d4605dba6d31d0bb4c2bbb2
Nane <kds.napha@gmail.com>
2026-02-18 14:49:57 +0900

    delete all file for refac

--- src/interfaces/http/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './controllers/auth.controller';
-import { RegisterUserUseCase } from '@application/use-cases/register-user.use-case';
-
-@Module({
-    controllers: [AuthController],
-    providers: [RegisterUserUseCase],
-})
-export class AuthModule { }
