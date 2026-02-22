6b7f97981955ea14bce2bcd785f2411ff54f5749
Nane <kds.napha@gmail.com>
2026-02-23 08:00:11 +0900

    call RegisterUserDto

--- src/modules/user/presentation/user.controller.ts
+++ src/modules/user/presentation/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { RegisterUserDto } from '../application/dto/register-user.dto';
 
 
 @Controller('users')
@@ -6,7 +7,7 @@ export class UserController {
 
     @Post('register')
     @HttpCode(HttpStatus.CREATED)
-    register(@Body() createUserDto: any) {
+    register(@Body() dto: RegisterUserDto) {
         // Implement user registration logic here
     }
 }
