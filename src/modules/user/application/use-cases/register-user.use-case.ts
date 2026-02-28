ef90ebc1c1885d10c1394212134b4a7702f8b8cb
Nane <kds.napha@gmail.com>
2026-02-28 17:58:18 +0900

    call userRepository

--- src/modules/user/application/use-cases/register-user.use-case.ts
+++ src/modules/user/application/use-cases/register-user.use-case.ts
@@ -1,11 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { RegisterUserDto } from "../dto/register-user.dto";
 import { UserResponseDto } from "../dto/user-response.dto";
+import { USER_REPOSITORY } from "../../domain/user-repository.interface";
+import type { IUserRepository } from "../../domain/user-repository.interface";
 
 @Injectable()
 export class RegisterUserUseCase {
+    constructor(
+        @Inject(USER_REPOSITORY)
+        private readonly userRepository: IUserRepository,
+    ) { }
 
     async execute(dto: RegisterUserDto): Promise<UserResponseDto> {
+        const existingUser = await this.userRepository.findByEmail(dto.email);
 
         return new UserResponseDto();
     }
