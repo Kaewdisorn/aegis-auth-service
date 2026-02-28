a99858c2b5a05e4fd2b56b1bb8682ccbf2b52a11
Nane <kds.napha@gmail.com>
2026-02-28 18:11:56 +0900

    update UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './presentation/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './domain/user.entity';
+import { USER_REPOSITORY } from './domain/user-repository.interface';
+import { TypeOrmUserRepository } from './infrastructure/typeorm-user.repository';
 import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
+import { UserController } from './presentation/user.controller';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
-    providers: [RegisterUserUseCase],
+    providers: [
+        RegisterUserUseCase,
+        {
+            provide: USER_REPOSITORY,
+            useClass: TypeOrmUserRepository,
+        },
+    ],
+    exports: [USER_REPOSITORY],
 })
 export class UserModule { }
