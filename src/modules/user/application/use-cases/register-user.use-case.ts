4fe9db462771c976d075a926f693d20cc0676498
Nane <kds.napha@gmail.com>
2026-02-28 16:51:25 +0900

    call registerUserUseCase

--- src/modules/user/application/use-cases/register-user.use-case.ts
+++ src/modules/user/application/use-cases/register-user.use-case.ts
@@ -1,1 +1,12 @@
-export class RegisterUserUseCase { }
+import { Injectable } from "@nestjs/common";
+import { RegisterUserDto } from "../dto/register-user.dto";
+import { UserResponseDto } from "../dto/user-response.dto";
+
+@Injectable()
+export class RegisterUserUseCase {
+
+    async execute(dto: RegisterUserDto): Promise<UserResponseDto> {
+
+        return new UserResponseDto();
+    }
+}
--- src/modules/user/presentation/user.controller.ts
+++ src/modules/user/presentation/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { RegisterUserDto } from '../application/dto/register-user.dto';
+import { RegisterUserUseCase } from '../application/use-cases/register-user.use-case';
 
 
 @Controller('users')
 export class UserController {
+    constructor(private readonly registerUserUseCase: RegisterUserUseCase) { }
 
     @Post('register')
     @HttpCode(HttpStatus.CREATED)
     register(@Body() dto: RegisterUserDto) {
-        // Implement user registration logic here
+        return this.registerUserUseCase.execute(dto);
     }
 }
