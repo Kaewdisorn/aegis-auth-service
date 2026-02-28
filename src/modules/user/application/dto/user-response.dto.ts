25f3705ef430f854e6cbbfedb83672505ebb9386
Nane <kds.napha@gmail.com>
2026-02-28 16:46:55 +0900

    create UserResponseDto

---
+++ src/modules/user/application/dto/user-response.dto.ts
@@ -0,0 +1,17 @@
+import { User } from "../../domain/user.entity";
+
+export class UserResponseDto {
+    gid: string;
+    uid: string;
+    email: string;
+    createdAt: Date;
+
+    static fromEntity(user: User): UserResponseDto {
+        const dto = new UserResponseDto();
+        dto.gid = user.gid;
+        dto.uid = user.uid;
+        dto.email = user.email;
+        dto.createdAt = user.createdAt;
+        return dto;
+    }
+}
