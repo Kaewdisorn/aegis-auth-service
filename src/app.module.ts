4d251184d45f3758bb3d8c0652b05bcb80a8acb5
Nane <kds.napha@gmail.com>
2026-02-28 17:38:43 +0900

    add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+  ],
 })
 export class AppModule { }
