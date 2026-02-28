9af6a60ebc17da8f87ce2bcfe8b047b02dac6c07
Nane <kds.napha@gmail.com>
2026-02-28 18:00:40 +0900

    done RegisterUserUseCase

--- src/modules/user/application/use-cases/register-user.use-case.ts
+++ src/modules/user/application/use-cases/register-user.use-case.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
 import { RegisterUserDto } from "../dto/register-user.dto";
 import { UserResponseDto } from "../dto/user-response.dto";
 import { USER_REPOSITORY } from "../../domain/user-repository.interface";
 import type { IUserRepository } from "../../domain/user-repository.interface";
+import { UserAlreadyExistsException } from "../../domain/exceptions/user-already-exists.exception";
 
 @Injectable()
 export class RegisterUserUseCase {
@@ -13,7 +15,17 @@ export class RegisterUserUseCase {
 
     async execute(dto: RegisterUserDto): Promise<UserResponseDto> {
         const existingUser = await this.userRepository.findByEmail(dto.email);
+        if (existingUser) {
+            throw new UserAlreadyExistsException(dto.email);
+        }
 
-        return new UserResponseDto();
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+        const user = await this.userRepository.save({
+            email: dto.email,
+            password: hashedPassword,
+        });
+
+        return UserResponseDto.fromEntity(user);
     }
 }
